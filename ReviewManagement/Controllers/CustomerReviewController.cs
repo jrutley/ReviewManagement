1f9603f919bd517e669a719d3f584d1f1d1020a4
Jeramy Rutley <jrutley@gmail.com>
2017-04-11 21:01:34 -0400

    Update whitespace

--- ReviewManagement/Controllers/CustomerReviewController.cs
+++ ReviewManagement/Controllers/CustomerReviewController.cs
@@ -20,18 +20,18 @@ namespace ReviewManagement.Controllers
     [HttpGet("[action]")]
     public IActionResult MyProductsAndReviews([FromQuery] string email) // Don't do this in a real app!
     {
-        var customer = _customerDb.Customers
-                .Include(c => c.Products)
-                .ThenInclude(cp => cp.Product)
-                .ThenInclude(p => p.Reviews)
-                .SingleOrDefault(c => c.Email == email);
-            if (customer == null) return Ok(new List<CustomerViewDTO>());
-            var productMapping = customer
-                .Products
-                .Select(cp => cp.Product)
-                .Select(product => { return new CustomerViewDTO { Product = product, Review = product.Reviews.SingleOrDefault(r => r.CustomerId == customer.CustomerId) }; }).ToList();
-            // Need to wrap this with a data property
-            var wrapped = new CustomerViewDTOWrapper { Data = productMapping };
+      var customer = _customerDb.Customers
+              .Include(c => c.Products)
+              .ThenInclude(cp => cp.Product)
+              .ThenInclude(p => p.Reviews)
+              .SingleOrDefault(c => c.Email == email);
+      if (customer == null) return Ok(new List<CustomerViewDTO>());
+      var productMapping = customer
+          .Products
+          .Select(cp => cp.Product)
+          .Select(product => { return new CustomerViewDTO { Product = product, Review = product.Reviews.SingleOrDefault(r => r.CustomerId == customer.CustomerId) }; }).ToList();
+      // Need to wrap this with a data property
+      var wrapped = new CustomerViewDTOWrapper { Data = productMapping };
       return Ok(wrapped);
     }
   }
