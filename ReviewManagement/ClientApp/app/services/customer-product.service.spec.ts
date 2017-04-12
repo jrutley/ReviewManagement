a5f46d99a7db9c5c660a7a544d6da80d0f0ca83f
Jeramy Rutley <jrutley@gmail.com>
2017-04-11 22:16:18 -0400

    Fixed up unit test to execute properly

--- ReviewManagement/ClientApp/app/services/customer-product.service.spec.ts
+++ ReviewManagement/ClientApp/app/services/customer-product.service.spec.ts
@@ -1,39 +1,36 @@
-import { async, inject, TestBed } from '@angular/core/testing';
+import { async, inject, TestBed, fakeAsync } from '@angular/core/testing';
 import { HttpModule, XHRBackend, Response, ResponseOptions } from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
 import { CustomerProductService } from './customer-product.service';
 
 const mockResponse = {
     data: [
-        { product: { name: 'Product 1' }, review: { comments: 'Sucks!'}}
+        { product: { name: 'Product 1' }, review: { comments: 'Sucks!' } }
     ]
 };
 
-describe('Customer-product: Service', () => {
+describe('Customer-product service\'s', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [HttpModule],
-            providers: [{ provide: XHRBackend, useClass: MockBackend }]
+            providers: [{ provide: XHRBackend, useClass: MockBackend }, CustomerProductService]
         });
     }));
 
-  describe('getProducts', () => {
-      it('should return a Promise<ProductViewModel>', () => {
-          inject([CustomerProductService, XHRBackend], (service, backend) => {
+    describe('getProducts', () => {
+        it('should return a Promise<ProductViewModel>',
+            async(inject([CustomerProductService, XHRBackend], (service, backend) => {
 
-              backend.connections.subscribe((connection) => {
-                  connection.mockRespond(new Response(new ResponseOptions({
-                      body: JSON.stringify(mockResponse)
-                  })));
-              });
+                backend.connections.subscribe((connection) => {
+                    connection.mockRespond(new Response(new ResponseOptions({
+                        body: JSON.stringify(mockResponse)
+                    })));
+                });
 
-
-              service.getProducts().then(p => {
-                  expect(p).toHaveBeenCalled();
-                  //expect(p[0]).toBe()
-                  expect(p).toThrowError("Finish me");
-              })
-          })
-      })
-  })
+                service.getProducts().then(p => {
+                    console.log(p);
+                    expect(p.data[0]).toBe("Product 1");
+                })
+            })));
+    })
 })
