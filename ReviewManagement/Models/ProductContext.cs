4839b27c73f9a3b18f3cd2eaca9932bbcc384a45
Jeramy Rutley <jrutley@gmail.com>
2017-04-12 22:50:36 -0400

    Update whitespace

--- ReviewManagement/Models/ProductContext.cs
+++ ReviewManagement/Models/ProductContext.cs
@@ -1,71 +1,70 @@
-using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ReviewManagement.Models
 {
-    public class Customer
+  public class Customer
+  {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int CustomerId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public ICollection<CustomerProduct> Products { get; set; }
+  }
+
+  public class CustomerProduct
+  {
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+  }
+
+  public class Product
+  {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public ICollection<Review> Reviews { get; set; }
+  }
+
+  public class Review
+  {
+    public int CustomerId { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int ReviewId { get; set; }
+
+    public int Stars { get; set; }
+
+    public string Comments { get; set; }
+  }
+
+  public class ProductContext : DbContext
+  {
+    public ProductContext(DbContextOptions<ProductContext> options)
+        : base(options)
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int CustomerId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
 
-        public ICollection<CustomerProduct> Products { get; set; }
     }
-    
-    public class CustomerProduct
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public int CustomerId { get; set; }
-        public Customer Customer { get; set; }
-
-        public int ProductId { get; set; }
-        public Product Product { get; set; }
+      // SQLite doesn't support n-m relations directly
+      modelBuilder.Entity<CustomerProduct>().HasKey(table => new { table.CustomerId, table.ProductId });
+      modelBuilder.Entity<Customer>().HasMany(c => c.Products).WithOne(c => c.Customer);
+      modelBuilder.Entity<Product>().HasMany(p => p.Reviews);
+      base.OnModelCreating(modelBuilder);
     }
 
-    public class Product
-    {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ProductId {get; set;}
-        public string Name {get; set;}
-        public string Description {get; set;}
-
-        public ICollection<Review> Reviews { get; set; }
-    }
-
-    public class Review 
-    {
-        public int CustomerId { get; set; }
-
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ReviewId { get; set; }
-
-        public int Stars {get;set;}
-
-        public string Comments {get;set;}
-    }
-
-    public class ProductContext : DbContext
-    {
-        public ProductContext(DbContextOptions<ProductContext> options)
-            : base(options)
-        {
-            
-        }        
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // SQLite doesn't support n-m relations directly
-            modelBuilder.Entity<CustomerProduct>().HasKey(table => new { table.CustomerId, table.ProductId });
-            modelBuilder.Entity<Customer>().HasMany(c => c.Products).WithOne(c => c.Customer);
-            modelBuilder.Entity<Product>().HasMany(p => p.Reviews);
-            base.OnModelCreating(modelBuilder);
-        }
-
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<CustomerProduct> CustomerProduct {get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Review> Reviews { get; set; }
-    }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<CustomerProduct> CustomerProduct { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+  }
 }
