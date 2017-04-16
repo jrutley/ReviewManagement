f419e91d28bcc51006e14aba3f9a03083bb113b9
Jeramy Rutley <jrutley@gmail.com>
2017-04-16 11:49:23 -0400

    Re-adding missing tests

--- ReviewManagement/ClientApp/app/components/customerReview/customerReview.component.spec.ts
+++ ReviewManagement/ClientApp/app/components/customerReview/customerReview.component.spec.ts
@@ -8,6 +8,8 @@ import { ProductViewModel } from '../customer/product.viewmodel';
 import { CustomerProductService } from '../../services/customer-product.service';
 import { Observable } from 'rxjs';
 
+class MockCustomerReviewService { }
+
 describe('customer review', () => {
   let fixture: ComponentFixture<CustomerReviewComponent>;
   const mockProduct = [{ name: "Mock 1", review: "Sucks!" }, { name: "Mock 2", review: "Awesome!" }, { name: "No review" }];
@@ -18,21 +20,56 @@ describe('customer review', () => {
         CustomerReviewComponent
       ],
       imports: [
-        //ReactiveFormsModule
+        ReactiveFormsModule
       ]
     })
       // Override component's own provider
       .overrideComponent(CustomerReviewComponent, {
         set: {
           providers: [
-            //{ provide: CustomerProductService, useClass: MockCustomerProductService }
+            { provide: CustomerProductService, useClass: MockCustomerReviewService }
           ]
         }
       });
   }));
+
   beforeEach(() => {
     fixture = TestBed.createComponent(CustomerReviewComponent);
-    fixture.componentInstance.product = { id: 1, name: "Mock 1", review: "Sucks!" };
+    fixture.componentInstance.product = { id: 1, name: "Spaceballs the video tape", review: "Never play it again" };
     fixture.detectChanges();
+  });
+
+  it('should display the product name', () => {
+    const span = fixture.debugElement.query(By.css('span'));
+    expect(span.nativeElement.textContent).toContain("Spaceballs the video tape");
+  });
+
+  describe('with existing review', () => {
+    beforeEach(() => {
+      fixture.componentInstance.product = { id: 1, name: "Spaceballs the toilet paper", review: "Nice and smooth" };
+      fixture.detectChanges();
+    });
+    it('should display the review', () => {
+      const spans = fixture.debugElement.queryAll(By.css('span'));
+      expect(spans[1].nativeElement.textContent).toContain("Nice and smooth");
+    });
+  })
+
+
+
+  it('should display the customer\'s existing reviews for that', fakeAsync(() => {
+    // const products = fixture.debugElement.queryAll(By.css('.customer-product'));
+    // expect(products.length).toBeGreaterThan(0);
+    // expect(products[0].nativeElement.textContent).toContain('Mock 1');
+  }));
+  it('should display an Add Review button when no review exists', () => {
+    // const button = fixture.debugElement.query(By.css('button'));
+    // expect(button.parent.children.find(el => el.nativeElement.textContent === 'No review'))
+  });
+
+  it('should not display an Add Review button when a review exists', () => {
+    // const products = fixture.debugElement.queryAll(By.css('li'));
+    // console.log(products);
+    // expect(products[0].query(By.css('button'))).toBe(null);
   });
 })
